cebb691a4305e7f39548c400be903e67e62eeeee
Anna <annprogram-gh@mail.ru>
2022-04-26 12:15:26 +0300

    clear project

---
+++ src/components/TheHeader.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="TheHeader">
+    <HeaderNaigation />
+  </div>
+</template>
+
+<script lang="ts">
+  import { defineComponent } from 'vue';
+  import HeaderNaigation from './TheHeaderNavigation.vue';
+
+  export default defineComponent({
+    name: 'TheHeader',
+    components: {
+      HeaderNaigation
+    }
+  });
+</script>
+
+<style lang='scss' scoped>
+
+</style>
---
+++ src/components/TheHeaderNavigation.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="TheHeaderNavigation">
+    
+  </div>
+</template>
+
+<script lang="ts">
+  import { defineComponent } from 'vue';
+
+  export default defineComponent({
+    name: 'TheHeaderNavigation',
+  });
+</script>
+
+<style lang='scss' scoped>
+
+</style>
